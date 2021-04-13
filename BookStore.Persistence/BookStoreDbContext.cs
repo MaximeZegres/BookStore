a90cb72f772ffcb8b2e26274796c08ad56205b45
Maxime Zegres <maxime.zegres@gmail.com>
2021-04-13 23:23:32 +0200

    Add Persistance project : Add EntityFrameworkCore and DbContext

---
+++ BookStore.Persistence/BookStoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Persistence
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
